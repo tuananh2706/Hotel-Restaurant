03fe63451b0add8f571f78e01f09377aacb7ecb6
maruk12 <hoangkhoi704@gmail.com>
2024-10-17 12:19:45 +0700

    uppdate password hash

---
+++ BE/Hotel_1/Hotel_1/Requests/AccountRequests.cs
@@ -0,0 +1,27 @@
+﻿namespace Hotel_1.Requests
+{
+	public class RegisterRequest
+	{
+		public string AccountName { get; set; }
+		public string Email { get; set; }
+		public string Password { get; set; }
+
+	}
+	public class LoginRequest
+		{
+			public string Email { get; set; }
+			public string Password { get; set; }
+		}
+	public class UpdateRequest
+	{
+		public string ?AccountName { get; set; }
+		public string ?FirstName { get; set; }
+		public string ?LastName { get; set; }
+		public string ?Email { get; set; }
+		public string ?Phone { get; set; }
+		public string ?Role { get; set; }
+		public string ?AvatarUrl { get; set; }
+	}
+}
+
+
